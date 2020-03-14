27b817683c9cc7872e3c1814808c569c5958e197
naimadswdn <39454533+naimadswdn@users.noreply.github.com>
2020-03-14 14:33:01 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args){
 	  System.out.println("---*** Hello World & my first java code ***---");
+	  System.out.println("Jenkins integration with git test");
+
 	}
 }
